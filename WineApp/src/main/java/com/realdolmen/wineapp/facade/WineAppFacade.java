46ab5811f6159fac126a5f36330bf2e63fd09702
Michael Verkoelen <Darkvader_9200@hotmail.com>
2019-01-14 10:43:03 +0100

    Startup

---
+++ WineApp/src/main/java/com/realdolmen/wineapp/facade/WineAppFacade.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.wineapp.facade;
+
+/**
+ *
+ * @author Michael
+ */
+public class WineAppFacade {
+    
+    
+    public static void main(String[] args) {
+        //startApp();
+    }
+}
